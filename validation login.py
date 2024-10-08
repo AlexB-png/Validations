c787c149ab1a9c73426c43d3b53af40c8494454c
AlexB-png <alexander.brankin.466@accesscreative.ac.uk>
2024-10-08 12:13:26 +0100

    Added comments and spaces

--- validation login.py
+++ validation login.py
@@ -1,14 +1,20 @@
-import os
-user=input("What is the username?\n")
-password=input("What is your password\n")
-os.system('CLS')
+import os #imports the terminal cleanser#
+
+user=input("What is the username?\n") #First input of Username#
+password=input("What is your password\n") #First input of password#
+
+os.system('CLS') #Clears the terminal#
+
 while user == "":
-    print("There was an error with your username")
-    user= input("What is the username\n")
-    os.system('CLS')
+    print("There was an error with your username") #Alert the user#
+    user= input("What is the username\n")#repeat the username#
+    os.system('CLS') #Clear the terminal#
+
 while password == "":
-    print("There was an error with your password")
-    password=input("What is your password \n")
-    os.system('CLS')
-os.system('CLS')
-print("Thank you for logging in", user)
+    print("There was an error with your password")#alert the user#
+    password=input("What is your password \n")#repeat the password#
+    os.system('CLS') #Clears terminal#
+
+os.system('CLS') #clear terminal#
+
+print("Thank you for logging in", user) #welcomes the user#
